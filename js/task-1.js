5e3f2d34508a2937d2a24713f24ec36bab317e97
Tokar Mykhailo <misatokar738@gmail.com>
2026-05-17 13:19:46 +0200

    update

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const categoriesList = document.querySelectorAll('.item');
-console.log(`Number of categories: ${categoriesList.length}`);
-
-categoriesList.forEach(category => {
-  const categoryName = category.querySelector('h2').textContent;
-  const elementsCount = category.querySelectorAll('li').length;
-  console.log(`Category: ${categoryName}`);
-  console.log(`Elements: ${elementsCount}`);
-});
